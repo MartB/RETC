a163037ad24539e8d7e5a7e089d17d07b01a6f95
MartB <mart.b@outlook.de>
2016-06-08 14:43:31 +0200

    Initial Commit.

---
+++ RETC/src/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+// Windows Header Files:
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <FCNTL.H>
+#include <io.h>
+#include <iostream>
+#include <algorithm>
+#include <future>
+#include <map>
+
+#pragma comment( lib, "rpcrt4")
+#include <rpc.h>
+#include <rpcdce.h>
